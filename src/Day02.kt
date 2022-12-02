d540f270d2594cdc23bf760d7bdaab846d0ec04a
TheGreatJakester <jacob.moulton@gmail.com>
2022-12-02 09:34:23 -0700

    Day 02 fail

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,6 +1,23 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val hands = input
+            .map { it.split(" ") }
+            .map { (theirs, yours) ->
+                val theirHand = Hand.values().first { it.theirs == theirs }
+                val yourHand = Hand.values().first { it.yours == yours }
+
+                val winScore = if (theirHand.beats == yourHand) {
+                    0
+                } else if (yourHand.beats == theirHand) {
+                    6
+                } else {
+                    3
+                }
+
+                winScore + yourHand.score
+            }
+
+        return hands.sum()
     }
 
     fun part2(input: List<String>): Int {
@@ -8,10 +25,32 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 13)
 
-    val input = readInput("Day01.txt")
-    println(part1(input))
+    val input = readInput("Day02")
+    println(part1(input.take(3)))
     println(part2(input))
+}
+
+
+sealed class Hand(val theirs: String, val yours: String, val score: Int, val beats: Hand) {
+    object Rock : Hand("A", "X", 1, Scissors)
+    object Paper : Hand("B", "Y", 2, Rock)
+    object Scissors : Hand("C", "Z", 3, Paper)
+
+    companion object {
+        fun values(): Array<Hand> {
+            return arrayOf(Rock, Paper, Scissors)
+        }
+
+        fun valueOf(value: String): Hand {
+            return when (value) {
+                "Rock" -> Rock
+                "Paper" -> Paper
+                "Scissors" -> Scissors
+                else -> throw IllegalArgumentException("No object Hand.$value")
+            }
+        }
+    }
 }
