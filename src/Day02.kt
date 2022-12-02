0ec26cc9239065e5635e7481148a387566200a37
TheGreatJakester <jacob.moulton@gmail.com>
2022-12-02 09:34:51 -0700

    Day 02

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,56 +1,51 @@
+import java.lang.Exception
+
+
+val possibleHands = mapOf(
+    "A X" to 4,
+    "B X" to 1,
+    "C X" to 7,
+
+    "A Y" to 8,
+    "B Y" to 5,
+    "C Y" to 2,
+
+    "A Z" to 3,
+    "B Z" to 9,
+    "C Z" to 6
+)
+
+val possibleHandsStrat2 = mapOf(
+    "A X" to 3,
+    "B X" to 1,
+    "C X" to 2,
+
+    "A Y" to 4,
+    "B Y" to 5,
+    "C Y" to 6,
+
+    "A Z" to 6+2,
+    "B Z" to 6+3,
+    "C Z" to 6+1
+)
 fun main() {
     fun part1(input: List<String>): Int {
-        val hands = input
-            .map { it.split(" ") }
-            .map { (theirs, yours) ->
-                val theirHand = Hand.values().first { it.theirs == theirs }
-                val yourHand = Hand.values().first { it.yours == yours }
-
-                val winScore = if (theirHand.beats == yourHand) {
-                    0
-                } else if (yourHand.beats == theirHand) {
-                    6
-                } else {
-                    3
-                }
-
-                winScore + yourHand.score
-            }
-
+        val hands = input.mapNotNull { possibleHands[it] }
         return hands.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val hands = input.mapNotNull { possibleHandsStrat2[it] }
+        return hands.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
-    check(part1(testInput) == 13)
+    check(part1(testInput) == 15)
+    check(part2(testInput) == 12)
 
     val input = readInput("Day02")
-    println(part1(input.take(3)))
+    println(part1(input))
     println(part2(input))
 }
 
-
-sealed class Hand(val theirs: String, val yours: String, val score: Int, val beats: Hand) {
-    object Rock : Hand("A", "X", 1, Scissors)
-    object Paper : Hand("B", "Y", 2, Rock)
-    object Scissors : Hand("C", "Z", 3, Paper)
-
-    companion object {
-        fun values(): Array<Hand> {
-            return arrayOf(Rock, Paper, Scissors)
-        }
-
-        fun valueOf(value: String): Hand {
-            return when (value) {
-                "Rock" -> Rock
-                "Paper" -> Paper
-                "Scissors" -> Scissors
-                else -> throw IllegalArgumentException("No object Hand.$value")
-            }
-        }
-    }
-}
