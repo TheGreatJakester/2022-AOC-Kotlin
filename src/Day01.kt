cd4107ff1c7b1667ee0b59c36c17cc71a11973c1
TheGreatJakester <jacob.moulton@gmail.com>
2022-12-02 09:33:59 -0700

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,34 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val elves = buildElfList(input)
+        return elves.maxOf { it.sum() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elves = buildElfList(input)
+        return elves.map { it.sum() }.sorted().takeLast(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+fun buildElfList(input: List<String>): List<List<Int>> {
+    val elves = mutableListOf<List<Int>>()
+    var currentList = mutableListOf<Int>()
+
+    input.forEach {
+        if (it.isEmpty()){
+            elves.add(currentList)
+            currentList = mutableListOf()
+        } else {
+            currentList.add(it.toInt())
+        }
+    }
+    return elves
 }
