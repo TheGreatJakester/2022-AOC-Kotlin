e66b4e6f083f7793ac431c6212533f6366ba060f
TheGreatJakester <jacob.moulton@gmail.com>
2022-12-06 11:31:52 -0700

    Refactor day 06

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,62 +1,20 @@
-import java.lang.Exception
-import java.util.Deque
-
 fun main() {
     object : Challenge<Int>("Day06", null, null) {
-        override fun Context.part1(): Int {
-            val text = fileContents
+        fun getFirstAllDifferent(count: Int, fileContents: String): Int{
+            val text = fileContents.toList()
 
-            val last4 = ArrayDeque<Char>()
-
-            repeat(4) {
-                last4.addFirst(' ')
+            val sizes = text.windowed(count){
+                it.toSet().size
             }
 
-            fun allDiff(): Boolean {
-                if (last4.size < 4) return false
-                val set = last4.toSet()
-                return set.size == 4
-            }
-
-            var index = 0
-
-            while (!allDiff() || index < 4) {
-                val next = text[index]
-                last4.addFirst(next)
-                last4.removeLast()
-                index += 1
-            }
-
-            return index
+            val index = sizes.indexOfFirst { it == count }
+            return index + count
         }
 
-        override fun Context.part2(): Int {
-            val text = fileContents
 
-            val last4 = ArrayDeque<Char>()
+        override fun Context.part1(): Int = getFirstAllDifferent(4, fileContents)
 
-            repeat(14) {
-                last4.addFirst(' ')
-            }
-
-            fun allDiff(): Boolean {
-                if (last4.size < 14) return false
-                val set = last4.toSet()
-                return set.size == 14
-            }
-
-            var index = 0
-
-            while (!allDiff() || index < 14) {
-                val next = text[index]
-                last4.addFirst(next)
-                last4.removeLast()
-                index += 1
-            }
-
-            return index
-        }
-
+        override fun Context.part2(): Int = getFirstAllDifferent(14, fileContents)
 
     }.trySolve()
 }
